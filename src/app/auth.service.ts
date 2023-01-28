f375440942987dfc7661ed5d18f79844e388c37c
Aaron Suydam <suydam.aaron@gmail.com>
2023-01-27 23:37:24 -0500

    Initial scaffolding for an HTTP request in authService

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,15 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { ServerRequestLogin } from './server-request-login';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+  loginSuccess: ServerRequestLogin | undefined;
+  loginUrl: string = "wahtever the heck the login url is.";
+  
 
   login(userName: string, userPassword: string): boolean
   {
     console.log(userName, userPassword);
-    return false;
+    this.getLoginSuccess(userName, userPassword).subscribe(data => this.loginSuccess =
+         {loginSuccess : (data as any).loginSuccess}); 
+    if(this.loginSuccess?.loginSuccess)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+  }
+
+  getLoginSuccess(userName: string, userPassword: string) {
+    return this.httpClient.get<ServerRequestLogin>(this.loginUrl);
+  }
+
+
+  interpret() {
+
   }
 }
