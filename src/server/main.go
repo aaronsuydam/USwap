5e4ed1f67435e70bf729ed5f0c50433956d483d2
Andrew7044 <80797345+Andrew7044@users.noreply.github.com>
2023-02-08 15:23:37 -0500

    Updated Backend, database should now work

--- src/server/main.go
+++ src/server/main.go
@@ -6,55 +6,43 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	"time"
 
 	"github.com/atxfjrotc/uswap/src/server/utils"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/gorilla/mux"
-	"github.com/rs/cors"
 )
 
 const (
-	username = "root"
-	password = "password"
-	hostname = "127.0.0.1:3306"
-	dbname   = "uswap"
+	username = "sql9595555"
+	password = "xXEPeR9JDl"
+	hostname = "sql9.freesqldatabase.com"
+	dbname   = "sql9595555"
 )
 
 func dsn(dbName string) string {
 	return fmt.Sprintf("%s:%s@tcp(%s)/%s", username, password, hostname, dbName)
 }
 
-func main() {
-	r := mux.NewRouter()
-
-	r.HandleFunc("/hello-world", helloWorld)
-
-	// Solver Cross Origin Access Issue
-	c := cors.New(cors.Options{
-		AllowedOrigins: []string{"http://localhost:4200"},
-	})
-	handler := c.Handler(r)
+func dbConnection() (*sql.DB, error) {
 	db, err := sql.Open("mysql", dsn(""))
 	if err != nil {
 		log.Printf("Error %s when opening DB\n", err)
-		return
+		return nil, err
 	}
-	defer db.Close()
+	//defer db.Close()
 
 	ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancelfunc()
 	res, err := db.ExecContext(ctx, "CREATE DATABASE IF NOT EXISTS "+dbname)
 	if err != nil {
 		log.Printf("Error %s when creating DB\n", err)
-		return
+		return nil, err
 	}
 	no, err := res.RowsAffected()
 	if err != nil {
 		log.Printf("Error %s when fetching rows", err)
-		return
+		return nil, err
 	}
 	log.Printf("rows affected %d\n", no)
 
@@ -62,9 +50,9 @@ func main() {
 	db, err = sql.Open("mysql", dsn(dbname))
 	if err != nil {
 		log.Printf("Error %s when opening DB", err)
-		return
+		return nil, err
 	}
-	defer db.Close()
+	//defer db.Close()
 
 	db.SetMaxOpenConns(20)
 	db.SetMaxIdleConns(20)
@@ -75,17 +63,61 @@ func main() {
 	err = db.PingContext(ctx)
 	if err != nil {
 		log.Printf("Errors %s pinging DB", err)
-		return
+		return nil, err
 	}
 	log.Printf("Connected to DB %s successfully\n", dbname)
+	return db, nil
+}
 
-	srv := &http.Server{
-		Handler: handler,
-		Addr:    ":" + os.Getenv("PORT"),
+func createProductTable(db *sql.DB) error {
+	query := `CREATE TABLE IF NOT EXISTS users(user_id int primary key, user_name text, user_email text,
+        product_price int)`
+	ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelfunc()
+
+	res, err := db.ExecContext(ctx, query)
+	if err != nil {
+		log.Printf("Error %s when creating user table", err)
+		return err
+	}
+
+	rows, err := res.RowsAffected()
+	if err != nil {
+		log.Printf("Error %s when getting rows affected", err)
+		return err
+	}
+
+	log.Printf("Rows affected when creating table: %d", rows)
+	return nil
+}
+
+func main() {
+	/*r := mux.NewRouter()
+
+	r.HandleFunc("/hello-world", helloWorld)
+
+	// Solver Cross Origin Access Issue
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:4200"},
+	})
+	handler := c.Handler(r)
+	*/
+	//BEGIN DATABASE CODE
+	db, err := dbConnection()
+	if err != nil {
+		log.Printf("Error %s when getting db connection", err)
+		return
+	}
+	defer db.Close()
+	log.Printf("Successfully connected to database")
+	err = createProductTable(db)
+	if err != nil {
+		log.Printf("Create user table failed with error %s", err)
+		return
 	}
 	//CREATE TABLE IF NOT EXISTS users(student_id int primary key, user_name text, user_email text, created_at datetime default CURRENT_TIMESTAMP, updated_at datetime default CURRENT_TIMESTAMP)
+	//END DATABASE CODE
 
-	log.Fatal(srv.ListenAndServe())
 }
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
