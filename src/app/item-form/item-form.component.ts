0d7a5345c0a5c497eea8025f60478bb5554598c2
erobx <erob7856@gmail.com>
2023-04-06 11:39:42 -0400

    validate item form and styling

--- src/app/item-form/item-form.component.ts
+++ src/app/item-form/item-form.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { StorageService } from '../services/storage.service';
 import { Router } from '@angular/router';
 
@@ -11,20 +11,30 @@ import { Router } from '@angular/router';
 })
 export class ItemFormComponent {
     form: FormGroup;
+    
     constructor(public fb: FormBuilder,
       private http: HttpClient,
       private storage: StorageService,
       private router: Router) {
 
         this.form = this.fb.group({
-          itemName: new FormControl(''),
-          itemDescription: new FormControl(''),
+          itemName: new FormControl('', [
+            Validators.required,
+          ]),
+          itemDescription: new FormControl('', [
+            Validators.required,
+          ]),
           userID: new FormControl(''),
           image: new FormControl(null),
         });
     }
 
     fileName: string = '';
+    fileUploaded: boolean = false;
+
+    get f() {
+      return this.form.controls;
+    }
 
     onFileSelected(event: any): void {
       const file: File = event.target.files[0];
@@ -37,22 +47,27 @@ export class ItemFormComponent {
         });
         this.form.get('image')?.updateValueAndValidity();
       }
+      this.fileUploaded = true;
     }
 
     addItem(): void {
-      var id = this.storage.getUser().id_token;
-      var formData: any = new FormData();
-      formData.append('itemName', this.form.controls['itemName'].value);
-      formData.append('itemDescription', this.form.controls['itemDescription'].value);
-      formData.append('userID', id);
-      formData.append('image', this.form.controls['image'].value);
-      
-      this.http.post("item/create", formData).subscribe({
-        next: (res) => console.log(res),
-        error: (err) => console.log(err),
-      });
+      if (this.fileUploaded && this.f['itemName'].value !== '') {
+        var id = this.storage.getUser().id_token;
+        var formData: any = new FormData();
+        formData.append('itemName', this.form.controls['itemName'].value);
+        formData.append('itemDescription', this.form.controls['itemDescription'].value);
+        formData.append('userID', id);
+        formData.append('image', this.form.controls['image'].value);
+        
+        this.http.post("item/create", formData).subscribe({
+          next: (res) => console.log(res),
+          error: (err) => console.log(err),
+        });
 
-      this.router.navigate(['./user-profile']);
+        this.router.navigate(['./user-profile']);
+      }
+
+      
     }
 
 }
