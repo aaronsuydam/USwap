8757c2e69253e4e85b152af639ae6b9cd623de4d
Aaron Suydam <suydam.aaron@gmail.com>
2023-04-07 16:52:11 -0400

    More Work on Homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { StorageService } from './services/storage.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   isLoggedIn = false;
